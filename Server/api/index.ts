d89df3c23fed8f909610b76757afd9fdbb665b9b
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2025-01-09 23:19:03 +0330

    befor

--- Server/api/index.ts
+++ Server/api/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 
-const app = new Hono().basePath('/')
+const app = new Hono().basePath('/api')
 
 app.get('/', (c) => {
   return c.json({ message: "Congrats! You've deployed Hono to Vercel" })
