a3b5cbafddcb7f36c3e80c5e0ed4d1cbb10c4bad
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2025-01-10 00:12:10 +0330

    finished

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,19 +1,9 @@
-import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
-import { serveStatic } from '@hono/node-server/serve-static'
 
 const app = new Hono()
 
-
-app.use('/static/*', serveStatic({ root: './' }))
-app.use('/BeautifulPeople.ttf', serveStatic({ path: '/static/BeautifulPeople.ttf' }))
-
-app.get('/', serveStatic({ path: '/static/index.html'}));
-
-const port = 3000
-console.log(`Server is running on port ${port}`)
-
-serve({
-  fetch: app.fetch,
-  port
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
 })
+
+export default app
