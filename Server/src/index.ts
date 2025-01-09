dadad035ca78fd23f9a62845a3a51b42176762c9
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2025-01-10 00:14:02 +0330

    hello hono

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('you hear my voice from CloudWlare workers and Hello Hono! 🚀' )
 })
 
 export default app
